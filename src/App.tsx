b7018d4044c5e32a0a67db55e7b6485eca00cba8
Otto Bossmann <otto.kostmann@hotmail.com>
2024-07-10 11:31:45 +0200

    Create nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 
 
 import './App.css'
+import {Nav} from './components/Nav'
 
 function App() {
   
 
   return (
     <>
-      
-     
+      <Nav/>
     </>
   )
 }
---
+++ src/components/Nav.tsx
@@ -0,0 +1,13 @@
+export function Nav(){
+return (
+    <>
+    <nav>
+        <ul>
+            <li>Logo</li>
+            <a href="http://www.github.com/saltsthlm"><li>Github</li></a>
+            
+        </ul>
+    </nav>
+    </>
+)
+}
