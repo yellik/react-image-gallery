b630ab30fde20af5f8b8bc437f48aad83e6c8dca
Yellik <yelenakensborn@gmail.com>
2024-07-11 16:11:23 +0200

    added mutation

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,10 +1,27 @@
 import { Image } from "./Image";
-
 import { useEffect, useState } from "react";
 import { SearchBar } from "./SearchBar";
-import { useQuery } from "@tanstack/react-query";
+import { QueryClient, useMutation, useQuery } from "@tanstack/react-query";
+
+
+
+const likePhoto = async (photoId) => {
+  const response = await fetch(`https://api.unsplash.com/photos/${photoId}/like`, {
+    method: 'POST',
+    headers: {
+      "Authorization": "5990YRBZafXkBNrlIIFlKu9p5SSMERGtV09WSZbS95Q",
+    },
+  });
+  if (!response.ok) {
+    throw new Error('Network response was not ok ' + response.statusText);
+  }
+  return response.json();
+};
+
+
 
 export function Board() {
+  const queryClient = new QueryClient();
   const { data, error, isLoading } = useQuery({
     queryKey: ["photo"],
     queryFn: () =>
@@ -13,6 +30,11 @@ export function Board() {
       ).then((res) => res.json()),
   });
 
+  
+  const mutation = useMutation({
+    mutationFn: likePhoto ,
+    onSuccess: () => {queryClient.invalidateQueries({queryKey: ['photo']})}
+  });
   console.log(data);
   if (isLoading) {
     return (
@@ -32,7 +54,10 @@ export function Board() {
       <SearchBar />
 
       {data.map((photo) => (
+        <div key={photo.id}>
         <Image key={photo.id} photo={photo} />
+        <button onClick={() => mutation.mutate(photo.id)}>Like Me</button>
+        </div>     
       ))}
     </>
   );
