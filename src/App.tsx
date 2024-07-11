611d140add46ae313a164701f125b9ee2182b152
Marcus Rådell <marcus@radell.net>
2024-07-11 14:10:10 +0200

    setup query client

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import "./App.css";
 import { Nav } from "./components/Nav";
 import { Board } from "./components/Board";
 import { Image } from "./components/Image";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <Nav />
-      <Board />
+      <QueryClientProvider client={queryClient}>
+        <Nav />
+        <Board />
+      </QueryClientProvider>
     </>
   );
 }
