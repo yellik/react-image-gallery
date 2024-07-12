762b1a87b15096c8185ebc82c5236e9090aac700
Otto Bossmann <otto.kostmann@hotmail.com>
2024-07-12 12:01:56 +0200

    aaaaa

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,37 +1,32 @@
-import { Image } from "./Image";
-import { SearchBar } from "./SearchBar";
-import { useQuery } from "@tanstack/react-query";
+import { Image } from './Image';
+import { SearchBar } from './SearchBar';
+import { useQuery } from '@tanstack/react-query';
 
-export function Board() {
-  //need to add prop for searchterm, also if no searchterm, send default get.
-  const fetchPhotosSearch = async (searchTerm:string) => {
+export function Board({ searchTerm }) {
+  const fetchPhotosSearch = async () => {
     const response = await fetch(
-      `https://api.unsplash.com/search/photos/?query=${searchTerm}`,
+      `https://api.unsplash.com/search/photos/?query=${searchTerm || 'default'}`,
       {
         headers: {
-          Authorization: "Client-ID 5990YRBZafXkBNrlIIFlKu9p5SSMERGtV09WSZbS95Q",
+          Authorization: 'Client-ID 5990YRBZafXkBNrlIIFlKu9p5SSMERGtV09WSZbS95Q',
         },
       }
     );
     if (!response.ok) {
-      throw new Error("Network response was not ok " + response.statusText);
+      throw new Error('Network response was not ok ' + response.statusText);
     }
     const data = await response.json();
     return data.results;
   };
 
   const { data, error, isLoading } = useQuery({
-    queryKey: ["photo"],
+    queryKey: ['photo', searchTerm],
     queryFn: fetchPhotosSearch,
   });
 
   console.log(data);
   if (isLoading) {
-    return (
-      <>
-        <h1>is Loading...</h1>
-      </>
-    );
+    return <h1>is Loading...</h1>;
   }
 
   if (error) {
@@ -42,7 +37,6 @@ export function Board() {
     <>
       <h1>Board</h1>
       <SearchBar />
-
       {data.map((photo) => (
         <div key={photo.id}>
           <Image key={photo.id} photo={photo} />
--- src/routes/search.tsx
+++ src/routes/search.tsx
@@ -1,9 +1,15 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { Board } from '../components/Board'
+import { createFileRoute } from '@tanstack/react-router';
+import { Board } from '../components/Board';
+import { useLocation } from '@tanstack/react-router';
+
+const SearchRouteComponent = () => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const searchTerm = searchParams.get('query') || '';
+
+  return <Board searchTerm={searchTerm} />;
+};
 
 export const Route = createFileRoute('/search')({
-  component: () => <div>
-    <Board/>
-  //need to add feature for prop drilling saerch searchTerm
-  </div>
-})
+  component: SearchRouteComponent,
+});
